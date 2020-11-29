3398c5f1af3a0b9fac2ba09b267106f87dce888d
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-11-29 15:23:14 -0600

    Add links

--- components/LanguageSelect.vue
+++ components/LanguageSelect.vue
@@ -2,6 +2,20 @@
   <div v-editable="blok" class="pb-8 mb-6 font-bold text-center">
     <h2 class="text-base">{{ blok.subheadline }}</h2>
     <h1 class="text-5xl">{{ blok.headline }}</h1>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link class="text-teal-600 hover:underline" to="/en/">
+            I speak English
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link class="text-teal-600 hover:underline" to="/ar/">
+            I speak Arabic
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
   </div>
 </template>
 
