acd60c1b57cf3f74259400f45281b43e7f67ee58
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-11-29 09:36:47 -0600

    match components to Storyblok components

---
+++ components/Grid.vue
@@ -0,0 +1,19 @@
+/* eslint-disable vue/no-template-shadow */
+<template>
+  <ul v-editable="blok" class="flex py-8 mb-6">
+    <li v-for="blok in blok.columns" :key="blok._uid" class="flex-auto px-6">
+      <component :is="blok.component" :blok="blok" />
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
