a7d964c606028541b46d6ac41d9435c83d49244f
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2020-11-29 14:13:26 -0600

    Add a language select component

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="max-w-5xl mx-auto py-8 flex">
-    <div class="flex-1">
+    <div class="content-start">
       <a href="/" class="block w-56 mx-auto">
         <img
           src="https://a.storyblok.com/f/98561/x/d36d4c52a8/logo-stillirise.svg"
---
+++ components/LanguageSelect.vue
@@ -0,0 +1,17 @@
+<template>
+  <div v-editable="blok" class="pb-8 mb-6 font-bold text-center">
+    <h2 class="text-base">{{ blok.subheadline }}</h2>
+    <h1 class="text-5xl">{{ blok.headline }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    blok: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
