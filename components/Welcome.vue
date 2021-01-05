048f8c044a4f656b460456f4535b3db93160f67a
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2021-01-04 18:46:52 -0600

    add text bloks

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -1,8 +1,14 @@
 <template>
-  <div v-editable="blok" class="pb-8 mb-6 font-bold text-center">
-    <h2 class="text-base">{{ blok.subheadline }}</h2>
-    <h1 class="text-5xl">{{ blok.headline }}</h1>
+  <div v-editable="blok" class="pb-8 mb-6 text-center">
+    <h2 class="text-base font-bold">{{ blok.subheadline }}</h2>
+    <h1 class="text-5xl font-bold">{{ blok.headline }}</h1>
     <div id="vrview"></div>
+    <p class="text-left">{{ blok.mission }}</p>
+    <p class="text-left">{{ blok.interaction_instruction }}</p>
+    <p class="text-left">{{ blok.audio_instruction }}</p>
+    <h1 class="text-5xl font-bold">{{ blok.prompt }}</h1>
+    <button>{{ blok.button_previous }}</button>
+    <button>{{ blok.button_next }}</button>
   </div>
 </template>
 
@@ -19,16 +25,6 @@ export default {
   },
   mounted() {
     window.addEventListener('load', this.onVrViewLoad)
-
-    // function onVrViewLoad() {
-    //   // Selector '#vrview' finds element with id 'vrview'.
-    //   // eslint-disable-next-line no-undef
-    //   const vrView = new VRView.Player('#vrview', {
-    //     image: this.blok.vrImage.filename,
-    //   })
-    //   // eslint-disable-next-line no-console
-    //   console.log(vrView)
-    // }
   },
   methods: {
     onVrViewLoad() {
