6bdb3a70e2491f70e4aa498d24dd0f530228d4b6
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2021-02-14 17:37:54 -0600

    add popup modal

--- components/LanguageListItem.vue
+++ components/LanguageListItem.vue
@@ -1,46 +1,56 @@
 <template>
-  <!-- <div
-    v-editable="blok"
-    class="flex items-center"
-    @click.prevent="dialogOpen = true"
-  > -->
-  <nuxt-link
+  <div>
+    <dialog :ref="blok.language" class="m-auto">
+      <div class="flex justify-center items-center flex-col flex-grow">
+        <h2 class="text-5xl font-bold pb-8">
+          {{ blok.language }}
+        </h2>
+        <menu class="p-0 flex flex-row justify-center">
+          <a
+            class="button m-2 w-12 h-9 p-2 rounded-lg"
+            @click.prevent="$refs[blok.language].close()"
+            >{{ blok.no }}</a
+          >
+          <nuxt-link
+            class="button m-2 w-12 h-9 p-2 rounded-lg"
+            :to="blok.page"
+            @click.native="blok.rtl ? isRtl : isLtr"
+            >{{ blok.yes }}</nuxt-link
+          >
+        </menu>
+      </div>
+    </dialog>
+    <a
+      v-editable="blok"
+      class="flex items-center"
+      @click.prevent="$refs[blok.language].showModal()"
+    >
+      <!-- <nuxt-link
     v-editable="blok"
     class="flex items-center"
     :to="blok.page"
     @click.native="blok.rtl ? isRtl : isLtr"
-  >
-    <audio controls class="hidden">
-      <source :src="blok.audio.filename" />
-      Your browser does not support the audio element.
-    </audio>
-    <img
-      class="button ml-3 w-12 h-9 p-2 rounded-lg"
-      src="/icons/volume_up-white-48dp.svg"
-      alt="listen to audio"
-    />
-    <p :class="{ rtl: blok.rtl }" class="flex-grow text-left pl-6">
-      {{ blok.language }}
-    </p>
-    <img
-      class=""
-      src="/icons/keyboard_arrow_right-black-48dp.svg"
-      alt="Continue in this language"
-    />
-    <dialog open :class="{ hidden: !dialogOpen }" class="m-auto">
-      <form>
-        <p>
-          {{ blok.language }}
-        </p>
-        <menu>
-          <nuxt-link :to="blok.page" @click.native="blok.rtl ? isRtl : isLtr">{{
-            blok.yes
-          }}</nuxt-link>
-          <button @click.prevent="dialogOpen = false">{{ blok.no }}</button>
-        </menu>
-      </form>
-    </dialog>
-  </nuxt-link>
+  > -->
+
+      <audio controls class="hidden">
+        <source :src="blok.audio.filename" />
+        Your browser does not support the audio element.
+      </audio>
+      <img
+        class="button ml-3 w-12 h-9 p-2 rounded-lg"
+        src="/icons/volume_up-white-48dp.svg"
+        alt="listen to audio"
+      />
+      <p :class="{ rtl: blok.rtl }" class="flex-grow text-left pl-6">
+        {{ blok.language }}
+      </p>
+      <img
+        class=""
+        src="/icons/keyboard_arrow_right-black-48dp.svg"
+        alt="Continue in this language"
+      />
+    </a>
+  </div>
 </template>
 
 <script>
@@ -50,11 +60,6 @@ export default {
       type: Object,
       required: true,
     },
-  },
-  data() {
-    return {
-      dialogOpen: false,
-    }
   },
   methods: {
     isRtl() {
@@ -86,6 +91,10 @@ dialog::backdrop {
   right: 0px;
   bottom: 0px;
   left: 0px;
-  background: rgba(0, 0, 0, 0.1);
+  background: rgba(0, 0, 0, 0.5);
+}
+dialog {
+  width: 100vw;
+  max-width: 500px;
 }
 </style>
