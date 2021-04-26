a5547c1de9479cf657d13e13e4dc505dcd704277
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2021-04-25 20:24:55 -0500

    troubleshoot button width

--- components/Welcome.vue
+++ components/Welcome.vue
@@ -27,9 +27,7 @@
         {{ blok.prompt }}
       </h1>
 
-      <a
-        href="./principles/"
-        class="text-xl text-white button ml-3 w-12 h-9 p-2 rounded-lg"
+      <a href="./principles/" class="text-xl text-white button p-2 rounded-lg"
         ><strong>{{ blok.button_next }}</strong></a
       >
     </div>
