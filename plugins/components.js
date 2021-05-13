c2e18affb8d589486e13af64520ac7b2e34673cf
Amelia Ruzek <amelia.ruzek.klein@gmail.com>
2021-05-12 20:08:34 -0500

    update components file as well to remove unneeded

--- plugins/components.js
+++ plugins/components.js
@@ -1,24 +1,19 @@
 import Vue from 'vue'
 import Page from '~/components/Page.vue'
-import Teaser from '~/components/Teaser.vue'
 import Grid from '~/components/Grid.vue'
-import Feature from '~/components/Feature.vue'
 import LanguageSelect from '~/components/LanguageSelect.vue'
 import LanguageListItem from '~/components/LanguageListItem.vue'
 import Welcome from '~/components/Welcome.vue'
 import Content from '~/components/Content.vue'
 import Checklist from '~/components/Checklist.vue'
 import ListItem from '~/components/ListItem.vue'
-import Image from '~/components/Image.vue'
 import Audio from '~/components/Audio.vue'
 import Title from '~/components/Title.vue'
 import ParagraphText from '~/components/ParagraphText.vue'
 import NextPageButton from '~/components/NextPageButton.vue'
 
 Vue.component('page', Page)
-Vue.component('teaser', Teaser)
 Vue.component('grid', Grid)
-Vue.component('feature', Feature)
 Vue.component('LanguageSelect', LanguageSelect)
 Vue.component('LanguageListItem', LanguageListItem)
 Vue.component('Welcome', Welcome)
@@ -26,7 +21,6 @@ Vue.component('Content', Content)
 Vue.component('Checklist', Checklist)
 Vue.component('ListItem', ListItem)
 Vue.component('Audio', Audio)
-Vue.component('Image', Image)
 Vue.component('Title', Title)
 Vue.component('ParagraphText', ParagraphText)
 Vue.component('NextPageButton', NextPageButton)
